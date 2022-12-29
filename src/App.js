6dded85015ac3d251eb0c7c90dd40a7cb24b7ddc
eviltik <eviltik@gmail.com>
2022-12-29 09:51:37 +0000

    comment xr and keyboard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+/*
 import { Controllers, Hands, VRButton, XR } from "@react-three/xr";
 import { KeyboardControls } from "@react-three/drei";
 import { Perf } from "r3f-perf";
+*/
 
 import Scene from "./components/Scene";
 import "./styles.css";
 
+/*
 const keyMap = [
   { name: "forward", keys: ["KeyW"] },
   { name: "backward", keys: ["KeyS"] },
@@ -16,22 +19,26 @@ const keyMap = [
   { name: "crouch", keys: ["ControlLeft"] },
   { name: "jump", keys: ["Space"] },
 ];
+*/
 
 export default function App() {
   return (
     <>
       {/* <VRButton /> */}
-      <KeyboardControls map={keyMap}>
-        <Canvas dpr={[1, 2]} shadows>
-          <Perf position="top-left" />
-          <XR>
-            <Controllers />
-            <Hands />
-            <Scene />
-          </XR>
-          <OrbitControls />
-        </Canvas>
-      </KeyboardControls>
+      {/* <KeyboardControls map={keyMap}> */}
+      <Canvas dpr={[1, 2]} shadows camera={{ fov: 45, position: [0, 5, -30] }}>
+        {/*
+        <Perf position="top-left" />
+        <XR>
+          <Controllers />
+          <Hands />
+          <Scene />
+        </XR>
+        */}
+        <Scene />
+        <OrbitControls target={[0, 2, 0]} />
+      </Canvas>
+      {/* </KeyboardControls> */}
     </>
   );
 }
