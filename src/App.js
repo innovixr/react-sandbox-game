dd356005c499a30734f869227d3140d02ace187c
eviltik <eviltik@gmail.com>
2022-12-31 13:36:28 +0100

    add leva

--- src/App.js
+++ src/App.js
@@ -1,22 +1,73 @@
 import { Canvas } from "@react-three/fiber";
 import Scene from "./components/Scene";
 import "./styles.css";
+import { Perf } from 'r3f-perf';
+import { folder, useControls } from 'leva';
+import { XR, Controllers, Hands, VRButton } from "@react-three/xr";
 
 export default function App() {
+
+  const lc = useControls({
+    'Debug':folder({
+      perfMonitor:false
+    }),
+    'Lights':folder({
+      'Directional':folder({
+        dirEnable:true,
+        dirIntensity:{ value: 0.5, min: 0, max: 30, step: 0.1 },
+        dirRadius:{ value: 4, min: 0, max:100, step:0.01}
+      }),
+      'Ambiant':folder({
+        ambEnable:true,
+        ambIntensity:{ value: 3, min: 0, max: 30, step: 0.1 },
+      })
+    })
+  });
+
   return (
     <>
-      {/* <VRButton /> */}
       <Canvas dpr={[1, 2]} shadows camera={{ fov: 45, position: [0, 10, 40] }}>
-        {/*
-        <Perf position="top-left" />
+        {
+          lc.perfMonitor && 
+            <Perf position="top-left" />
+        }
+        {
+          /*
+          <XR>
+            <Controllers />
+            <Hands />
+            <Scene />
+          </XR>
+          */
+        }
+        {
+          lc.dirEnable && 
+            <directionalLight
+              intensity={lc.dirIntensity}
+              castShadow={true}
+              shadow-bias={-0.00015}
+              shadow-radius={4}
+              shadow-blur={10}
+              shadow-mapSize={[2048, 2048]}
+              position={[85.0, 80.0, 70.0]}
+              shadow-camera-left={-30}
+              shadow-camera-right={30}
+              shadow-camera-top={30}
+              shadow-camera-bottom={-30}
+            />
+        }
+
+        {
+          lc.ambEnable &&
+            <ambientLight color={ 0x909090 } intensity={lc.ambIntensity} />
+        }
         <XR>
           <Controllers />
           <Hands />
           <Scene />
         </XR>
-        */}
-        <Scene />
       </Canvas>
+      <VRButton />
     </>
   );
 }
--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,5 +1,5 @@
 import { RigidBody } from "@react-three/rapier";
-import { useEffect, useRef/*, useState*/ } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Debug, Physics } from "@react-three/rapier";
 import { Map } from './Map.js';
 import { extend, useFrame, useThree } from '@react-three/fiber';
@@ -8,23 +8,47 @@ import { Skybox } from "./Skybox";
 import { PointerLockControls } from "three/examples/jsm/controls/PointerLockControls";
 import { Crosshair } from "./Crosshair.js";
 import { Mirror } from "./Mirror.js";
-import { PerspectiveCamera } from '@react-three/drei'
+import { PerspectiveCamera } from '@react-three/drei';
+import { folder, useControls } from 'leva';
+import { useKeyboardInput } from "./hooks/useKeyboardInput";
+import { useVariable } from "./hooks/useVariable";
+import * as THREE from 'three';
+
+//import { softShadows } from "@react-three/drei";
 
 extend({ PointerLockControls });
 
+/*
+softShadows({
+  frustum: 3.75,
+  size: 0.005,
+  near: 9.5,
+  samples: 17,
+  rings: 11, // Rings (default: 11) must be a int
+})
+*/
+
 const rr = 1;
 const rf = 1;
-//const MAX_SPHERE = 20;
+const MAX_SPHERE = 100;
+let lastInsertedSphere = 0;
 
-/*
 function randomIntFromInterval(min, max) { // min and max included 
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
-*/
 
 export default function Scene({ clicked }) {
 
   const { camera, gl } = useThree();
+  const pressed = useKeyboardInput(["KeyB"]);
+  const input = useVariable(pressed);
+
+  const levaControls = useControls({
+    'Debug':folder({
+      debugPhysics:false,
+      addBallons:false
+    })
+  });
 
   useEffect(() => {
     camera.layers.enable(0);
@@ -40,88 +64,73 @@ export default function Scene({ clicked }) {
   }, [gl]);
 
 
-  //let timeCount = 0;
-  //const [spheres, setSpheres] = useState([]);
+  let timeCount = 0;
+  const [spheres, setSpheres] = useState([]);
 
   const controls = useRef();
 
   useFrame((state, delta) => {
-    /*
-    timeCount+=delta;
-    if (timeCount>1 && spheres.length < MAX_SPHERE) {
-      timeCount = 0;
+
+    const { KeyB } = input.current;
+    const nis = lastInsertedSphere + (1*500);
+    const canInsertSphere = Date.now() > nis;
+
+    if (KeyB && canInsertSphere) {
       const kname='rs'+spheres.length;
-      setSpheres([
-        ...spheres,
-        <RigidBody key={kname} colliders="ball" restitution={rr} friction={rf}>
-          <mesh position={[ randomIntFromInterval(-1,1), 20, randomIntFromInterval(-1,1)]}>
-            <sphereGeometry/>
-            <meshStandardMaterial color="darkorange"/>
-          </mesh>
-        </RigidBody>
-      ])
+      let color = new THREE.Color( 0xffffff );
+      color.setHex( Math.random() * 0xffffff );
+        setSpheres([
+          ...spheres,
+          <RigidBody key={kname} colliders="ball" restitution={rr} friction={rf}>
+            <mesh 
+              castShadow
+              receiveShadow
+              position={[ randomIntFromInterval(-1,1), 20, randomIntFromInterval(-1,1)]}>
+              <sphereGeometry args={[0.5]}/>
+              <meshStandardMaterial color={color}/>
+            </mesh>
+          </RigidBody>
+        ])
+      lastInsertedSphere = Date.now();
     }
-    */
+
+
+    if (levaControls.addBallons) {
+      timeCount+=delta;
+      if (timeCount>2 && spheres.length < MAX_SPHERE) {
+        timeCount = 0;
+        const kname='rs'+spheres.length;
+        setSpheres([
+          ...spheres,
+          <RigidBody key={kname} colliders="ball" restitution={rr} friction={rf}>
+            <mesh 
+              castShadow
+              receiveShadow
+              position={[ randomIntFromInterval(-1,1), 20, randomIntFromInterval(-1,1)]}>
+              <sphereGeometry args={[0.5]}/>
+              <meshStandardMaterial color="darkorange"/>
+            </mesh>
+          </RigidBody>
+        ])
+      }
+    }
   })
 
   return (
     <>
-      <directionalLight
-        intensity={1}
-        castShadow={true}
-        shadow-bias={-0.00015}
-        shadow-radius={4}
-        shadow-blur={10}
-        shadow-mapSize={[2048, 2048]}
-        position={[85.0, 80.0, 70.0]}
-        shadow-camera-left={-30}
-        shadow-camera-right={30}
-        shadow-camera-top={30}
-        shadow-camera-bottom={-30}
-      />
-
-      <ambientLight color={ 0x909090 } intensity={1} />
       <Skybox/>
       <pointerLockControls ref={controls} args={[camera, gl.domElement]}/>
+
       <PerspectiveCamera makeDefault position={[0, 0, 0]}>
-        <Crosshair position={[0,0,-1]}/>
+        <Crosshair position={[0,0,-0.1]}/>
       </PerspectiveCamera>
+      
       <Mirror position={[0, 1, 5.2]}/>
       <Physics timeStep="vary">
-        <Debug/>
+        { levaControls.debugPhysics && <Debug/> }
         <Map position={[0, 0, 0]}/>
-        <RigidBody colliders="ball" restitution={rr} friction={rf}>
-          <mesh position={[10.5, 10, 10]}>
-            <sphereGeometry/>
-            <meshStandardMaterial color="darkorange"/>
-          </mesh>
-        </RigidBody>
-        <RigidBody colliders="ball" restitution={rr} friction={rf}>
-          <mesh position={[12.5, 11, 10]}>
-            <sphereGeometry/>
-            <meshStandardMaterial color={0xFF0000}/>
-          </mesh>
-        </RigidBody>
-        <RigidBody colliders="ball" restitution={rr} friction={rf}>
-          <mesh position={[13.5, 12, 10]}>
-            <sphereGeometry/>
-            <meshStandardMaterial color={0xFF0000}/>
-          </mesh>
-        </RigidBody>
-        <RigidBody colliders="ball" restitution={rr} friction={rf}>
-          <mesh position={[15.5, 13, 10]}>
-            <sphereGeometry/>
-            <meshStandardMaterial color={0x0000FF}/>
-          </mesh>
-        </RigidBody>
-        <RigidBody colliders="ball" restitution={rr} friction={rf}>
-          <mesh position={[16.5, 14, 10]}>
-            <sphereGeometry/>
-            <meshStandardMaterial color={0x00FF00}/>
-          </mesh>
-        </RigidBody>
         <Player />
-        { /* [...spheres] */ }
+        { [...spheres] }
       </Physics>
     </>
   )
